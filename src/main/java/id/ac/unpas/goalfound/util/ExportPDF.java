d5a6631a6a2bc65c9111f1683b127dc36f856e70
Chalidarlh <chalidarlh@gmail.com>
2026-01-03 23:52:33 +0700

    crud data pemain

---
+++ src/main/java/id/ac/unpas/goalfound/util/ExportPDF.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.goalfound.util;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import javax.swing.JTable;
+import java.io.FileOutputStream;
+/**
+ *
+ * @author Asus
+ */
+public class ExportPDF {
+
+    public static void exportTable(JTable table, String fileName) throws Exception {
+        Document doc = new Document();
+        PdfWriter.getInstance(doc, new FileOutputStream(fileName));
+        doc.open();
+
+        PdfPTable pdfTable = new PdfPTable(table.getColumnCount());
+
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            pdfTable.addCell(table.getColumnName(i));
+        }
+
+        for (int r = 0; r < table.getRowCount(); r++) {
+            for (int c = 0; c < table.getColumnCount(); c++) {
+                pdfTable.addCell(table.getValueAt(r, c).toString());
+            }
+        }
+
+        doc.add(pdfTable);
+        doc.close();
+    }
+}
