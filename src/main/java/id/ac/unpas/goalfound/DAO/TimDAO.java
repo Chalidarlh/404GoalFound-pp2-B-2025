5d2fe68e455ec16ffb0ce7fc8074e24c5178f011
falen23 <byzad252@gmail.com>
2026-01-06 16:30:30 +0700

    menambahkan fitur Search Tim

--- src/main/java/id/ac/unpas/goalfound/DAO/TimDAO.java
+++ src/main/java/id/ac/unpas/goalfound/DAO/TimDAO.java
@@ -44,5 +44,14 @@ public class TimDAO {
         ps.setInt(1, id);
         ps.executeUpdate();
     }
+    
+    public ResultSet search(String keyword) throws Exception {
+    String sql = "SELECT * FROM tim WHERE nama_tim LIKE ? OR fakultas LIKE ?";
+    PreparedStatement ps = KoneksiDB.configDB().prepareStatement(sql);
+    ps.setString(1, "%" + keyword + "%");
+    ps.setString(2, "%" + keyword + "%");
+    return ps.executeQuery();
+}
+
 }
 
